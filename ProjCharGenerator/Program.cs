c10349dbe2f2e14fa5bad6059831233532ed4fbb
AlinaYausheva <43522405+AlinaYausheva@users.noreply.github.com>
2023-04-24 00:19:44 +0300

    Update Program.cs

--- ProjCharGenerator/Program.cs
+++ ProjCharGenerator/Program.cs
@@ -1,45 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
+using System.IO;
 
 namespace generator
 {
-    class CharGenerator 
-    {
-        private string syms = "абвгдеёжзийклмнопрстуфхцчшщьыъэюя"; 
-        private char[] data;
-        private int size;
-        private Random random = new Random();
-        public CharGenerator() 
-        {
-           size = syms.Length;
-           data = syms.ToCharArray(); 
-        }
-        public char getSym() 
-        {
-           return data[random.Next(0, size)]; 
-        }
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            CharGenerator gen = new CharGenerator();
-            SortedDictionary<char, int> stat = new SortedDictionary<char, int>();
-            for(int i = 0; i < 1000; i++) 
+            string file_name;
+
+            GeneratorBigrams gen1 = new GeneratorBigrams();
+            file_name = "TextByBigrams.txt";
+            file_name = Path.Combine(Path.GetFullPath(AppDomain.CurrentDomain.BaseDirectory + @"..\\..\\..\\"), file_name);
+            StreamWriter file1 = new StreamWriter(file_name, false);
+            int i = 0;
+            while (i < 1000)
             {
-               char ch = gen.getSym(); 
-               if (stat.ContainsKey(ch))
-                  stat[ch]++;
-               else
-                  stat.Add(ch, 1); Console.Write(ch);
+                char sym = gen1.getSym();
+                i ++;
+
+                file1.Write(sym);
             }
-            Console.Write('\n');
-            foreach (KeyValuePair<char, int> entry in stat) 
+            file1.Close();
+
+            GeneratorWords gen2 = new GeneratorWords();
+            file_name = "TextByWords.txt";
+            file_name = Path.Combine(Path.GetFullPath(AppDomain.CurrentDomain.BaseDirectory + @"..\\..\\..\\"), file_name);
+            StreamWriter file2 = new StreamWriter(file_name, false);
+            i = 0;
+            while (i < 1000)
             {
-                 Console.WriteLine("{0} - {1}",entry.Key,entry.Value/1000.0); 
+                string sym = gen2.getSym();
+                i ++;
+
+                file2.Write(sym + " ");
             }
-            
-        }
+            file2.Close();
+
+            GeneratorWordPairs gen3 = new GeneratorWordPairs();
+            file_name = "TextByWordPairs.txt";
+            file_name = Path.Combine(Path.GetFullPath(AppDomain.CurrentDomain.BaseDirectory + @"..\\..\\..\\"), file_name);
+            StreamWriter file3 = new StreamWriter(file_name, false);
+            i = 0;
+            while (i < 1000)
+            {
+                string ch = gen3.getSym();
+                i +=2;
+
+                file3.Write(ch + " ");
+            }
+            file3.Close();
+        }   
     }
 }
-
