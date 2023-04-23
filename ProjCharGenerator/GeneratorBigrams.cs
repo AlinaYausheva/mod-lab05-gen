73eaa877485106a480f83b0d154b77e258bcdc4f
AlinaYausheva <43522405+AlinaYausheva@users.noreply.github.com>
2023-04-24 00:20:45 +0300

    Add files via upload

---
+++ ProjCharGenerator/GeneratorBigrams.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+
+namespace generator
+{
+    public class GeneratorBigrams
+    {
+        private string syms = "абвгдежзийклмнопрстуфхцчшщыьэюя";
+        private char[] data;
+        int[,] weights;
+        private int size;
+
+        private Random random = new Random();
+        private int index = 0;
+        public GeneratorBigrams()
+        {
+            size = syms.Length;
+            data = syms.ToCharArray();
+            weights = new int[size, size];
+            string file_name = "Bigrams.txt";
+            file_name = Path.Combine(Path.GetFullPath(AppDomain.CurrentDomain.BaseDirectory + @"..\\..\\..\\"), file_name);
+            StreamReader file = new StreamReader(file_name, false);
+            int i = 0;
+            string line;
+            while ((line = file.ReadLine()) != null)
+            {
+                string[] num = line.Split(' ');
+                for (int j = 0; j < size; j++)
+                {
+                    weights[i, j] = int.Parse(num[j]);
+                }
+                i++;
+            }
+            file.Close();
+        }
+
+        public char getSym()
+        {
+            int summa = 0;
+            int[] np = new int[size];
+            int[] letterWeights = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                letterWeights[i] = weights[index, i];
+                summa += weights[index, i];
+                np[i] = summa;
+            }
+
+            int m = random.Next(0, summa);
+            int index2;
+
+            for (index2 = 0; index2 < size; index2++)
+            {
+                if (m < np[index2])
+                    break;
+            }
+
+            index = index2;
+            return data[index2];
+        }
+    }
+}
+
---
+++ ProjCharGenerator/GeneratorWords.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+
+namespace generator
+{
+    public class GeneratorWords
+    {
+        private string[] data;
+        int[] weights;
+        private int size;
+
+        private Random random = new Random();
+
+        public GeneratorWords()
+        {
+            size = 100;
+            data = new string[size];
+            weights = new int[size];
+            string file_name = "WordFrequencies.txt";
+            file_name = Path.Combine(Path.GetFullPath(AppDomain.CurrentDomain.BaseDirectory + @"..\\..\\..\\"), file_name);
+            StreamReader file = new StreamReader(file_name, false);
+            int i = 0;
+            string line;
+            while ((line = file.ReadLine()) != null)
+            {
+                string[] numbers = line.Split(' ');
+                data[i] = numbers[0];
+                weights[i] = int.Parse(numbers[1]);
+                i++;
+            }          
+        }
+
+        public string getSym()
+        {
+            int summa = 0;
+            int[] np = new int[size];
+            int index;
+            for (int i = 0; i < size; i++)
+            {
+                summa += weights[i];
+                np[i] = summa;
+            }           
+            int m = random.Next(0, summa);
+            for (index = 0; index < size; index++)
+            {
+                if (m < np[index])
+                    break;
+            }
+            return data[index];
+        }
+    }
+}
